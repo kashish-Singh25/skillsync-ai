48b2f30e15e1d1dc18f1d977052b563f08ffa472
kashish singh <kashishsingh@192.168.1.3>
2026-07-28 02:54:44 +0530

    fix cors for production frontend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+
+app.use(
+    cors({
+      origin: "https://skillsync-ai-2-nfh0.onrender.com",
+      credentials: true,
+    })
+  );
 app.use(express.json());
 
 app.use((req, res, next) => {
